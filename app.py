ed07dd5a8c538c7b6c0b7c9e7292b2c0f67f9195
EC2 Default User <ec2-user@ip-172-31-9-101.us-west-2.compute.internal>
2021-05-01 01:28:53 +0000

    working prototype

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, escape, redirect, request
 from flask_bootstrap import Bootstrap
 from flask_colorpicker import colorpicker
+from datetime import datetime
+import uuid
+import json
+import os
+import random
 
 app = Flask(__name__, template_folder='templates')
 Bootstrap(app)
@@ -8,8 +13,32 @@ colorpicker(app)
 
 @app.route('/')
 def home():
-    return render_template('index.html', test='abc', color='#a000a0')
+    try:
+        thoughts = os.listdir('thoughts')
+        filename = random.choice(thoughts)
+        
+        with open('thoughts/{}'.format(filename)) as f:
+            data = f.read()
+
+        thought_info = json.loads(data)
+        thought, color = thought_info['thought'], thought_info['color']
+
+    except IndexError:
+        thought, color = 'I got an internship I\'m hyped for!', '#ff4500'
+
+    return render_template('index.html', thought=thought, color=color)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    thought, color = escape(request.form['thought']), escape(request.form['color'])
+
+    thought_filename = str(uuid.uuid4())
+
+    with open('thoughts/{}'.format(thought_filename), 'w') as f:
+        f.write(json.dumps({'thought': thought, 'color': color, 'stamp': str(datetime.now())}))
+
+    return redirect('/')
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port='8080')
